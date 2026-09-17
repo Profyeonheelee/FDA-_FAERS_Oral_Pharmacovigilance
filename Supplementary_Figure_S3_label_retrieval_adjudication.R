db2edc046dc5e7976f449d133a5f9680f4c4b00f
Profyeonheelee <omod0209@gmail.com>
2026-09-17 16:01:19 -0400

    Delete Supplementary_Figure_S3_label_retrieval_adjudication.R

--- Supplementary_Figure_S3_label_retrieval_adjudication.R
+++
@@ -1,4 +0,0 @@
-# Supplementary Figure S3 entry point.
-
-project_root <- Sys.getenv("FAERS_PROJECT_ROOT", unset = getwd())
-source(file.path(project_root, "code", "final_figures", "support", "Figure_S3_label_retrieval_adjudication.R"), chdir = TRUE)
