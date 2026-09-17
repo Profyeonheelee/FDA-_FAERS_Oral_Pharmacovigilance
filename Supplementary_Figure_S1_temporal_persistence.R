1f77994be5e68a199aa5dab055c869b135333eb6
Profyeonheelee <omod0209@gmail.com>
2026-09-17 16:00:54 -0400

    Delete Supplementary_Figure_S1_temporal_persistence.R

--- Supplementary_Figure_S1_temporal_persistence.R
+++
@@ -1,4 +0,0 @@
-# Supplementary Figure S1 entry point.
-
-project_root <- Sys.getenv("FAERS_PROJECT_ROOT", unset = getwd())
-source(file.path(project_root, "code", "final_figures", "support", "Figure_S1_temporal_persistence.R"), chdir = TRUE)
